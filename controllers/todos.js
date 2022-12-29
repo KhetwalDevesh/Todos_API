a37af3bf24da8f0436a7a849c4634c1970c8f58c
khetwaldevesh <deveshkhetwal05@gmail.com>
2022-12-29 21:42:18 +0530

    initial commit

--- controllers/todos.js
+++ controllers/todos.js
@@ -3,8 +3,8 @@ const Todos = require('../models/Todos');
 // get all Todos
 exports.getTodos = async (req, res, next) => {
   try {
-    // const todos = await Todos.find();
-    res.status(200).json({ success: true, data: 'get all todos' });
+    const todos = await Todos.find();
+    res.status(200).json({ success: true, data: todos });
   } catch (error) {
     res.status(400).json({ success: false });
     console.log(error);
@@ -27,7 +27,7 @@ exports.createTodo = async (req, res, next) => {
     const todo = await Todos.create(req.body);
     res.status(201).json({ success: true, data: todo });
   } catch (error) {
-    res.status(400).json({ success: false });
+    res.status(400).json({ success: false, error: error });
   }
 };
 
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 const app = express();
 dotenv.config({ path: './.env' });
+
+// connect with database
+connectDB();
+
 const todos = require('./routes/todos');
 const PORT = process.env.PORT;
 app.listen(PORT, (req, res) => {
   console.log(`server running on port ${PORT}`);
 });
 app.use('/api/v1/todos', todos);
+// app.get('/api/v1/todos', (req, res) => {
+//   res.status(200).send('hello');
+//   console.log('success get');
+// });
